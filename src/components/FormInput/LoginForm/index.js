9b89ae8c91125dc3b5a2984d722e671964de89d4
Dat Bac Do <dat.b.do@gmail.com>
2018-06-18 10:50:04 -0400

    Login screen is functional

---
+++ src/components/FormInput/LoginForm/index.js
@@ -0,0 +1,83 @@
+import React from "react";
+import { View, Text } from "react-native";
+import PropTypes from "prop-types";
+
+import Icon from "react-native-vector-icons/FontAwesome";
+import { Input, Button } from "react-native-elements";
+
+import styles from "./style";
+
+class LoginForm extends React.Component {
+    // This will declare the type of each object passed in this class
+    static propTypes = {
+        onLogin: PropTypes.func,
+        onSignup: PropTypes.func,
+        displaySignup: PropTypes.bool,
+        usernameErrorMessage: PropTypes.string,
+        passwordErrorMessage: PropTypes.string,
+    };
+    // This will declare all the default properties passed in this class
+    static defaultProps = {
+        onLogin: () => {},
+        onSignup: () => {},
+        displaySignup: false,
+        usernameErrorMessage: "",
+        passwordErrorMessage: "",
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "",
+            password: "",
+        };
+    }
+
+    render() {
+        const {
+            displaySignup,
+            onLogin,
+            onSignup,
+            usernameErrorMessage,
+            passwordErrorMessage,
+        } = this.props;
+
+        return (
+            <View style={styles.container}>
+                <Input
+                    placeholder="Username"
+                    onChangeText={username => this.setState({ username })}
+                    leftIcon={<Icon name="user" size={16} color="black" />}
+                    containerStyle={styles.usernameInputContainer}
+                    errorMessage={usernameErrorMessage}
+                />
+                <Input
+                    secureTextEntry
+                    placeholder="Password"
+                    onChangeText={password => this.setState({ password })}
+                    leftIcon={<Icon name="lock" size={16} color="black" />}
+                    containerStyle={styles.passwordInputContainer}
+                    errorMessage={passwordErrorMessage}
+                />
+                <View style={{ flexDirection: "row" }}>
+                    <Button
+                        title="Login"
+                        onPress={() =>
+                            onLogin(this.state.username, this.state.password)
+                        }
+                        containerStyle={styles.loginButton}
+                    />
+                    {displaySignup && (
+                        <Button
+                            title="Sign Up"
+                            onPress={onSignup}
+                            containerStyle={styles.signupButton}
+                        />
+                    )}
+                </View>
+            </View>
+        );
+    }
+}
+
+export default LoginForm;
