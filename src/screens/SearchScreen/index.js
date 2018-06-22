990a9279fd4e0b969a297b0e4d516b5cb04bd140
Sidney Prytherch <sidneyprytherch@gmail.com>
2018-06-22 18:27:16 -0400

    add search screen

--- src/screens/SearchScreen/index.js
+++ src/screens/SearchScreen/index.js
@@ -1,11 +1,26 @@
 import React from "react";
 import { View, Text, Button } from "react-native";
+import { SearchBar } from "react-native-elements";
+import styles from "./style";
 
 class SearchScreen extends React.Component {
     render() {
         const { navigate } = this.props.navigation;
         return (
-            <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+            <View
+                style={{
+                    width: "100%",
+                    flex: 1,
+                    justifyContent: "center",
+                }}
+            >
+                <SearchBar
+                    lightTheme
+                    searchIcon={{ size: 24 }}
+                    placeholder="Type Here..."
+                    style={styles.searchBarStyle}
+                    round
+                />
                 <Text>Other Screen</Text>
                 <Button
                     onPress={() => {
---
+++ src/screens/SearchScreen/style.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    searchBarStyle: {
+        backgroundColor: "red",
+        borderColor: "red",
+    },
+});
+
+export default styles;
