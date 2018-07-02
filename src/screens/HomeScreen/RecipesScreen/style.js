0a9920ef77444b10d83b0c94d7e7603874e96c8e
Sidney Prytherch <sidneyprytherch@gmail.com>
2018-07-02 12:03:48 -0400

    rename incorrectly named name

---
+++ src/screens/HomeScreen/RecipesScreen/style.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    flatListStyle: {
+        height: "80%",
+    },
+    viewStyle: {
+        height: "100%",
+    },
+    buttonGroupStyle: {
+        height: 40,
+        marginTop: 8,
+    },
+});
