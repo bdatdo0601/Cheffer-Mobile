2d47e738e3e8c395d88c023ea6df5dc84e89f9c4
Sidney Prytherch <sidneyprytherch@gmail.com>
2018-07-09 15:08:38 -0400

    separate default recipe data from Recipe component

--- src/components/Recipe/index.js
+++ src/components/Recipe/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { View, Text } from "react-native";
+import recipeDefaultData from "./recipeDefault";
 
 class Recipe extends React.Component {
     // This will declare the type of each object passed in this class
@@ -9,60 +10,7 @@ class Recipe extends React.Component {
     };
     // This will declare all the default properties passed in this class
     static defaultProps = {
-        data: {
-            image_url:
-                "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg",
-            name: "bread rolls",
-            ingredients: [
-                {
-                    ingredient: "schmeckles",
-                    amount: 5,
-                    measurement: "silver",
-                },
-                {
-                    ingredient: "food",
-                    amount: 2,
-                    measurement: "oz.",
-                },
-            ],
-            steps: [
-                "put your left foot in",
-                "take your left foot out",
-                "in",
-                "out",
-                "in",
-                "out",
-                "shake it all about",
-            ],
-            associatedRecipes: ["fa6i", "gfa76a"],
-            comments: [
-                {
-                    id: "fnsyoe",
-                    userId: "nduie",
-                    comment: "This wasn't bad",
-                    reply: [
-                        {
-                            id: "26tr29g",
-                            userId: "2f3vf",
-                            comment: "I agree",
-                            reply: [],
-                            likes: -10,
-                            time: "some date format",
-                        },
-                        {
-                            id: "2ot2o",
-                            userId: "fba697e",
-                            comment: "Really? I thought it was terrible",
-                            reply: [],
-                            likes: 32,
-                            time: "some date format",
-                        },
-                    ],
-                    likes: 256,
-                    time: "some date format",
-                },
-            ],
-        },
+        data: recipeDefaultData,
     };
 
     constructor(props) {
