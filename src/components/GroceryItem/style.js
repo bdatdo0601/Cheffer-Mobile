28aa2b96616491cabad20926a6d36cf4daac0b22
Dat Bac Do <dat.b.do@gmail.com>
2018-07-25 08:28:11 -0400

    fix the checkbox misalign

--- src/components/GroceryItem/style.js
+++ src/components/GroceryItem/style.js
@@ -4,13 +4,21 @@ export default StyleSheet.create({
     image: { height: 50, width: 70 },
     title: {
         fontWeight: "800",
+        flexGrow: 1,
     },
     info: {
         marginLeft: 16,
+        flexGrow: 20,
+    },
+    checkbox: {
+        flexGrow: 1,
+        alignContent: "center",
+        justifyContent: "center",
     },
     container: {
         flex: 1,
         flexDirection: "row",
-        paddingRight: 4,
+        justifyContent: "space-between",
+        alignItems: "center",
     },
 });
